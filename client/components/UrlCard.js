7537d7e3d6e0db01dae1a03f46cbf79d739fbbd5
nikolaev_pavel <nikolaev_pavel@aol.com>
2021-02-12 19:57:08 +0300

    add copy icon

--- client/components/UrlCard.js
+++ client/components/UrlCard.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
+import { AuthContext } from '../context/authContext'
 
 export default function UrlCard({ url }) {
+  const { me } = useContext(AuthContext)
+  const shortUrlRef = useRef(null)
+
+  const onCopyClick = async () => {
+    const shortUrl = shortUrlRef.current.innerText
+    await window.navigator.clipboard.writeText(shortUrl)
+  }
+
   return (
     <article className="uk-margin-top uk-card uk-card-default">
       <section className="uk-card-header">
@@ -13,14 +22,34 @@ export default function UrlCard({ url }) {
           {url.long}
         </a>
       </section>
-      <section className="uk-card-header ">
+      <section className="uk-card-footer uk-flex-inline">
         <a
           href={`${window.location.origin}/${url.short}`}
           target="_blank"
           rel="noreferrer"
+          ref={shortUrlRef}
         >
           {`${window.location.origin}/${url.short}`}
         </a>
+        <section className="uk-flex uk-flex-stretch uk-margin-left">
+          <i
+            className="material-icons"
+            title="Copy"
+            onClick={onCopyClick}
+          >
+            content_copy
+          </i>
+          {me && (
+            <>
+              <i
+                className="material-icons"
+                title="Delete"
+              >
+                delete_forever
+              </i>
+            </>
+          )}
+        </section>
       </section>
     </article>
   )
